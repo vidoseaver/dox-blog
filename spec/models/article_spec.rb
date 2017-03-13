afd6ce843e5f0bf9de22e2c04596b79688ed8912
Vido <vidoseaver@gmail.com>
2017-03-13 14:52:39 -0600

    Add tests for searching body and title functionality

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -13,5 +13,41 @@ feature "article model spec" do
       expect(articles.all? {|article| article.title.include?("Cancer")}).to eq true
       expect(articles.none? {|article| article.id == non_cancer_article.id}).to eq true
     end
+
+    it "returns all articles where body includes a string " do
+      author = create(:author)
+
+      cancer_article_one = author.articles.create!(title: "Blah Blah surviror", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      cancer_article_two = author.articles.create!(title: "Blah  Blah non-surviror", featured: false, body: "Little Bo-Pepe  survided cancer - not", published: true)
+      cancer_article_three = author.articles.create!(title: "Blah Blah almost surviror", featured: false, body: "Cancer survided little Bo-pepe. Almost.", published: true)
+      non_cancer_article = author.articles.create!(title: "Blah Blah not spoken of almost surviror", featured: false, body: "That which can not be spoken of did not survive survided little Bo-pepe", published: true)
+
+      articles = Article.where_title_or_body_includes("Cancer")
+
+      expect(articles.count).to eq(3)
+      expect(articles.all? {|article| true if article.body.include?("Cancer") || article.body.include?("cancer") }).to eq true
+      expect(articles.none? {|article| article.id == non_cancer_article.id}).to eq true
+    end
+    it "returns all articles where title or body includes a string " do
+      author = create(:author)
+
+      cancer_article_one = author.articles.create!(title: "Blah Blah Cancer surviror", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      cancer_article_two = author.articles.create!(title: "Blah Cancer Blah non-surviror", featured: false, body: "Little Bo-Pepe  survided cancer - not", published: true)
+      cancer_article_three = author.articles.create!(title: "Blah Blah almost surviror", featured: false, body: "Cancer survided little Bo-pepe. Almost.", published: true)
+      non_cancer_article = author.articles.create!(title: "Blah Blah not spoken of almost surviror", featured: false, body: "That which can not be spoken of did not survive survided little Bo-pepe", published: true)
+
+      articles = Article.where_title_or_body_includes("Cancer")
+
+      expect(articles.count).to eq(3)
+      expect( articles.all? do |article|
+        true if article.title.include?("Cancer") ||
+        article.body.include?("Cancer") ||
+        article.body.include?("cancer")
+      end).to eq true
+      expect(articles.none? {|article| article.id == non_cancer_article.id}).to eq true
+    end
+  end
+  context "text" do
+
   end
 end
