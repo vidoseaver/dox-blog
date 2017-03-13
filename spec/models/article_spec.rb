e86865b471f3be0f3a2f552713f1d786ec672df5
Vido <vidoseaver@gmail.com>
2017-03-13 14:32:24 -0600

    Returns all artciles where title includes a string - passing

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,14 +5,13 @@ feature "article model spec" do
       author = create(:author)
       cancer_article_one = author.articles.create!(title: "Blah Blah Cancer surviror", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
       cancer_article_two = author.articles.create!(title: "Blah Cancer Blah non-surviror", featured: false, body: "Little Bo-Pepe  survided cancer - not", published: true)
-      cancer_article_one = author.articles.create!(title: "Blah Blah almost surviror", featured: false, body: "Cancer survided little Bo-pepe. Almost.", published: true)
       non_cancer_article = author.articles.create!(title: "Blah Blah not spoken of almost surviror", featured: false, body: "That which can not be spoken of did not survive survided little Bo-pepe", published: true)
 
       articles = Article.where_title_or_body_includes("Cancer")
 
-      expect(articles.count).to eq(3)
-      expect(articles.all? {|article| arcticle.title.include?(Cancer)}).to eq true
-      expect(artciles).to not_include(non_cancer_article )
+      expect(articles.count).to eq(2)
+      expect(articles.all? {|article| article.title.include?("Cancer")}).to eq true
+      expect(articles.none? {|article| article.id == non_cancer_article.id}).to eq true
     end
   end
 end
