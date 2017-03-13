68dd847b82e2ba6cc29367575c2710b21cdc20d6
Vido <vidoseaver@gmail.com>
2017-03-13 14:30:00 -0600

    Add Class method to article where_where_title_or_body_includes

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,9 @@ class Article < ActiveRecord::Base
 
   scope :published, -> { where(published: true).order("created_at desc") }
   scope :featured, -> { where(published: true).where(featured: true).order("id desc") }
+
+  def self.where_title_or_body_includes(text)
+    where("title OR body LIKE ?", "%#{text}%")
+  end
+
 end
