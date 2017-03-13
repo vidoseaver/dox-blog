babee676fb83131e486a77ce6df0e746e208fda8
Vido <vidoseaver@gmail.com>
2017-03-13 15:30:47 -0600

    Add test Article Sorter class method count_of_appearances_in_title

---
+++ spec/poros/article_sorter_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Article_sorter" do
+  context ".count_of_apprearences_in_title(word)" do
+    it "returns the amount of times a word appears in a title" do
+      author = create(:author)
+      article = author.articles.create!(title: "Blah Blah Cancer surviror", featured: false, body: "Random words", published: true)
+
+      expect(ArticleSorter.count_of_apprearences_in_title(article, "Cancer")).to eq(1)
+    end
+  end
+end
