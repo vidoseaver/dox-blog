209f9129ec91cf0e1ac0263d9e577a176e655313
Vido <vidoseaver@gmail.com>
2017-03-13 16:17:33 -0600

    Add articles with scores class method

--- app/models/article_sorter.rb
+++ app/models/article_sorter.rb
@@ -5,4 +5,12 @@ class ArticleSorter
     section.scan(/(?=#{word.downcase})/).count
   end
 
+  def self.articles_with_scores(list_of_articles, word)
+    list_of_articles.map do |article|
+      title_score = self.count_of_apprearences(article, "title", word) * 2
+      body_score = self.count_of_apprearences(article, "body", word)
+      [article, title_score + body_score]
+    end
+  end
+
 end
