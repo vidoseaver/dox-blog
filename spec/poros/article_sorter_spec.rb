938b6f03fdd39f2b07b76167607173ca13ca484b
Vido <vidoseaver@gmail.com>
2017-03-13 15:49:34 -0600

    Adjust tests for Article sorter to reflect change to method count_of_appearances

--- spec/poros/article_sorter_spec.rb
+++ spec/poros/article_sorter_spec.rb
@@ -6,19 +6,19 @@ describe "Article_sorter" do
       author = create(:author)
       article = author.articles.create!(title: "Blah Blah Cancer surviror", featured: false, body: "Random words", published: true)
 
-      expect(ArticleSorter.count_of_apprearences_in_title(article, "Cancer")).to eq(1)
+      expect(ArticleSorter.count_of_apprearences(article, "title", "Cancer")).to eq(1)
     end
     it "returns the amount of times a word appears in a title and doesn't care about capitalization" do
       author = create(:author)
       article = author.articles.create!(title: "Blah blah BLAH Cancer surviror", featured: false, body: "Random words", published: true)
 
-      expect(ArticleSorter.count_of_apprearences_in_title(article, "Blah")).to eq(3)
+      expect(ArticleSorter.count_of_apprearences(article, "title", "Blah")).to eq(3)
     end
     it "is now dynamic and can can count appearance in body as well" do
       author = create(:author)
       article = author.articles.create!(title: "Blah blah BLAH Cancer surviror", featured: false, body: "Words WORDS words", published: true)
 
-      expect(ArticleSorter.count_of_apprearences_in_title(article, "body", "words")).to eq(3)
+      expect(ArticleSorter.count_of_apprearences(article, "body", "words")).to eq(3)
     end
   end
 end
