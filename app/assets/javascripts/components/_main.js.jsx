be92ebda31ccd7139eca81b230170ce7d09d721b
Vido <vidoseaver@gmail.com>
2017-03-14 12:52:28 -0600

    Add all articles request spec

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -2,7 +2,23 @@ var Main = React.createClass({
   render() {
     return (
       <div>
-      <h1> "HELLO"</h1>  
+        <div className='row'>
+          <div className='section'>
+            <div className='col-3-4'>
+              <h1>Articles</h1>
+            </div>
+            <div className='col-1-4'>
+              <h1>Search</h1>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='section'>
+            <div className='pagination'>
+              <h1>Pagination</h1>
+            </div>
+          </div>
+        </div>        
       </div>
     )
   }
---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "api/v1/articles" do
+  context "get to #index" do
+    it "returns json for all articles" do
+      author_one, author_two = create_list(:author, 2)
+      author_one.articles.create!(title: "Author one Article one", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      author_one.articles.create!(title: "Author one Article two", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      author_two.articles.create!(title: "Author two Article one", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      author_two.articles.create!(title: "Author two Article two", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+
+      get api_v1_articles_path
+
+      articles = JSON.parse(response.body, symbolize_names:true)
+
+      expect(response).to be_success
+      expect(articles.first.count).to eq 4
+    end
+  end
+end
