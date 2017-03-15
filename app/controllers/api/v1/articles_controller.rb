c4486ffd09cc836cd542cbe7bc31f01ae4c497f1
Vido <vidoseaver@gmail.com>
2017-03-15 11:00:15 -0600

    Change paginating around so it works via params

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
-    articles = Article.all
-    render json: articles, each_serializer: ArticleSerializer
+    articles = ArticleSorter.paginate(Article.all, 5)
+    render json: articles[params[:page].to_i], each_serializer: Api::V1::ArticleSerializer
   end
 end
