ab3c9bfea6aee11faf17baeffdbaa91d053fcfcf
Vido <vidoseaver@gmail.com>
2017-03-15 12:49:11 -0600

    search working

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -2,12 +2,26 @@ var Main = React.createClass({
 
 
   getInitialState() {
-    return { pageArticles: [], allArticles: []  }
+    return { pageArticles: [], titles: [], page: 0, searchWord: undefined, potentialWord: undefined  }
   },
 
   componentDidMount() {
     $.getJSON('/api/v1/articles?page=0', (response) => { this.setState({pageArticles: response}) });
+    $.getJSON('/api/v1/article_titles', (response) => { this.setState({titles: response}) });
   },
+
+  searchAndSetPages() {
+    var searchWord = $('#_title1').val()
+    $('#_title1').val('')
+
+    $.getJSON('/api/v1/articles_title_search?search=' + searchWord + '&page=0', (response) => { this.setState({pageArticles: response}) });
+    this.setState({searchWord: searchWord, page:0})
+  },
+
+  // filterAndSetPotentialWord(){
+  //
+  // }
+
 
 
   render() {
@@ -19,7 +33,13 @@ var Main = React.createClass({
               <PageArticles articles={this.state.pageArticles} />
             </div>
             <div className='col-1-4'>
-              <h1>Search</h1>
+              <div className='search'>
+                Search Articles
+                <p>
+                  <input type='text' name="[title]" id='_title1' value={this.state.potentialWord} onChange={filterAndSetPotentialWord}/>
+                  <input type='submit' name = 'commit' value='Search' onClick={this.searchAndSetPages}/>
+                </p>
+              </div>
             </div>
           </div>
         </div>
---
+++ app/assets/javascripts/components/_search.js.jsx
@@ -0,0 +1,12 @@
+var Search =  React.createClass({
+
+
+  render(){
+    return(
+      <div>
+        'Search Articles'
+      </div>
+      
+    )
+  }
+})
