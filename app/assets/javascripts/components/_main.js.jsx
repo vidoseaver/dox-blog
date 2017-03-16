bee2b5b766ad752aab59c49ce37eb8cf80255e84
Vido <vidoseaver@gmail.com>
2017-03-15 22:48:31 -0600

    Pagination working - could use refactor, not dynamic with search

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -11,17 +11,18 @@ var Main = React.createClass({
     $.getJSON('/api/v1/articles_count', (response) => { this.setState({pageCount: parseInt(response)}) });
   },
 
-  searchAndSetPages() {
+  searchAndSetPages(page = 1) {
     var searchWord = $('#_title').val()
     $('#_title1').val('')
+
     if (searchWord.length > 0) {
-      $.getJSON('/api/v1/articles_title_search?search=' + searchWord + '&page=0', (response) => { this.setState({pageArticles: response}) });
+      $.getJSON('/api/v1/articles_title_search?search=' + searchWord + '&page=' + (page - 1), (response) => { this.setState({pageArticles: response}) });
       $.getJSON('/api/v1/articles_count?search=' + searchWord, (response) => { this.setState({pageCount: parseInt(response)}) });
-      this.setState({searchWord: searchWord, currentPage:0})
+      this.setState({searchWord: searchWord, currentPage: page})
     } else {
-      $.getJSON('/api/v1/articles?page=0', (response) => { this.setState({pageArticles: response}) });
+      $.getJSON('/api/v1/articles?page=' + (page - 1) , (response) => { this.setState({pageArticles: response}) });
       $.getJSON('/api/v1/articles_count', (response) => { this.setState({pageCount: parseInt(response)}) });
-      this.setState({currentPage:0})
+      this.setState({currentPage: page})
     }
   },
 
@@ -29,29 +30,36 @@ var Main = React.createClass({
     cleaned = this.state.titles.map(function(article){
       return article.title;
     })
-    this.setState({titles: cleaned})
+    this.setState({titles: cleaned});
   },
 
-  updatePage(){
-
+  updatePage(pageNumber){
+    var newPage = parseInt(pageNumber);
+    this.searchAndSetPages(parseInt(newPage))
+    this.setState({currentPage: newPage});
+  },
+  changePageByOne(amount){
+    var newPage = parseInt(this.state.currentPage) + amount;
+    this.searchAndSetPages(newPage)
+    this.setState({currentPage: newPage});
   },
 
   createDataList() {
     var dataList = document.getElementById('matches-datalist')
-    this.state.titles.forEach((match) => {
-      var option = document.createElement('option')
-      option.value = match
-      dataList.appendChild(option)
+    this.state.titles.forEach(function(match) {
+      var option = document.createElement('option');
+      option.value = match;
+      dataList.appendChild(option);
     })
   },
 
 
   render() {
     if (this.state.titles.length > 0 && typeof this.state.titles[0] != 'string') {
-      this.cleanTitles()
+      this.cleanTitles();
     }
     if (typeof this.state.titles[0] == 'string' ) {
-      this.createDataList()
+      this.createDataList();
     }
 
     return (
@@ -59,7 +67,7 @@ var Main = React.createClass({
         <div className='row'>
           <div className='section'>
             <div className='pagination'>
-              <Pagination currentPage={this.state.currentPage} pageCount={this.state.pageCount} updatePage={this.updatePage}/>
+              <Pagination currentPage={this.state.currentPage} pageCount={this.state.pageCount} updatePage={this.updatePage} changePageByOne={this.changePageByOne}/>
             </div>
           </div>
         </div>
