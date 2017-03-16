7f62d6ebfff484bd8a03b0247d0ce78f131207ac
Vido <vidoseaver@gmail.com>
2017-03-15 19:07:05 -0600

    Change count to page count instead of artcle

--- app/controllers/api/v1/articles_count_controller.rb
+++ app/controllers/api/v1/articles_count_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::ArticlesCountController < ApplicationController
   def index
     if params[:search]
-      render json: Article.where_title_or_body_includes(params[:search]).count
+      articles = ArticleSorter.paginate(Article.where_title_or_body_includes(params[:search]), 5)
+      render json: articles.count
     else
-      render json: Article.count
+      render json: ArticleSorter.paginate(Article.all, 5).count
     end
   end
 end
--- spec/requests/articles_count_spec.rb
+++ spec/requests/articles_count_spec.rb
@@ -9,12 +9,15 @@ describe "api/v1/articles_count" do
       author_one.articles.create!(title: "Author one Article two", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
       author_two.articles.create!(title: "Author two Article one", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
       author_two.articles.create!(title: "Author two Article two", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      author_two.articles.create!(title: "Author two Article two", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      author_two.articles.create!(title: "Author two Article two", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
+      author_two.articles.create!(title: "Author two Article two", featured: false, body: "Little Bo-Pepe survided cancer", published: true)
 
       get api_v1_articles_count_index_path
 
 
       expect(response).to be_success
-      expect(response.body).to eq "4"
+      expect(response.body).to eq "2"
 
     end
     it "returns count for all articles searched" do
@@ -28,7 +31,7 @@ describe "api/v1/articles_count" do
 
 
       expect(response).to be_success
-      expect(response.body).to eq "3"
+      expect(response.body).to eq "1"
 
     end
   end
