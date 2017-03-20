39c1924eff0b9444e002882ce1e6a855d7b6cb5e
Vido <vidoseaver@gmail.com>
2017-03-20 12:50:41 -0600

    Change if else so that it accomadtes disabled nexts

---
+++ app/assets/javascripts/components/next_gap_or_prev.js.jsx
@@ -0,0 +1,27 @@
+var NextGapOrPrev = React.createClass({
+
+  handleClick(event){
+    var amount =  event.target.innerHTML === "Next →" ? 1 : - 1
+    this.props.changePageByOne(amount);
+  },
+
+  render(){
+    var pageDirection = this.props.pageDirection
+
+
+    // moved pageClassName into if else to avoid breaking if "..." less dry but just as effiecent?
+    // could break apart if elseif into two if and keep it drier...
+
+    if (pageDirection == "...") {
+      return (<span classname="gap">...</span> )
+    } else if (this.props.disabled) {
+      var direction =  pageDirection.split(" ")[1].toLowerCase() == "previous" ? "previous" : "next"
+      var pageClassName =  direction + "_page"
+      return (<span className={pageClassName + " disabled"} >{pageDirection}</span>)
+    } else {
+      var direction =  pageDirection.split(" ")[1].toLowerCase() == "previous" ? "previous" : "next"
+      var pageClassName =  direction + "_page"
+      return (<a className={pageClassName} onClick={this.handleClick}>{pageDirection}</a>)
+    }
+  }
+})
