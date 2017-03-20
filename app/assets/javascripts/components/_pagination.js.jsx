e86cd2e2ffb2446000dd2ce66ed15479ff1e7a98
Vido <vidoseaver@gmail.com>
2017-03-20 13:07:55 -0600

    Refactor pagination into less if else and one long render

--- app/assets/javascripts/components/_pagination.js.jsx
+++ app/assets/javascripts/components/_pagination.js.jsx
@@ -3,143 +3,69 @@ var Pagination = React.createClass({
   handleClick(pageNumber){
     this.props.updatePage(pageNumber);
   },
-  changePageByOne(event){
-    var amount =  event.target.innerHTML === "Next →" ? 1 : - 1
+  changePageByOne(amount){
     this.props.changePageByOne(amount);
   },
-  renderDotsFromZero() {
+
+  renderPagination(startCount, endCount) {
+    var pageCount   = parseInt(this.props.pageCount)
+    var currentPage = this.props.currentPage
     var dots = []
-    for (var i = 1; i < 10; i++) {
-      if (this.props.currentPage == 0 && this.props.currentPage == i - 1) {
+
+    if (currentPage <= 1) {
+      dots.push(<NextGapOrPrev disabled={true} pageDirection="← Previous" />)
+    } else  if ( currentPage > 8){
+      dots.push(<NextGapOrPrev changePageByOne={this.changePageByOne} pageDirection="← Previous" />)
+      dots.push(<a onClick={this.handleClick}>1</a>)
+      dots.push(<a onClick={this.handleClick}>2</a>)
+      dots.push(<span className="gap">...</span>)
+    } else {
+      dots.push(<NextGapOrPrev changePageByOne={this.changePageByOne}  pageDirection="← Previous" />)
+    }
+
+    for (var i = startCount; i < endCount; i++) {
+      if (currentPage == 0 && currentPage == i - 1) {
+        var dot = <PageDot className="current" pageNumber={i}/>
+      } else if (currentPage == i) {
         var dot = <PageDot className="current" pageNumber={i}/>
       } else {
         var dot = <PageDot handleClick={this.handleClick} pageNumber={i}/>
       }
       dots.push(dot)
     }
-    dots.push(<span className="gap">...</span>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount -1}</a>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount}</a>)
-    return(dots)
-  },
 
-  renderDotsUnderEight() {
-    var dots = [];
-    for (var i = 1; i < 10; i++) {
-      if (this.props.currentPage == i) {
-        var dot = <PageDot className="current" pageNumber={i}/>
-        dots.push(dot)
-      } else {
-        var dot = <PageDot handleClick={this.handleClick} pageNumber={i}/>
-        dots.push(dot)
-      }
-    }
-    dots.push(<span className="gap">...</span>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount -1}</a>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount}</a>)
-    return(dots)
-  },
-  renderDotsUnderEight() {
-    var dots = []
-    for (var i = 1; i < 10; i++) {
-      if (this.props.currentPage == i) {
-        var dot = <PageDot className="current" pageNumber={i}/>
-        dots.push(dot)
-      } else {
-        var dot = <PageDot handleClick={this.handleClick} pageNumber={i}/>
-        dots.push(dot)
-      }
-    }
-    dots.push(<span className="gap">...</span>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount -1}</a>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount}</a>)
-    return(dots)
-  },
-  renderDotsAtNine() {
-    var pageCount   = parseInt(this.props.pageCount)
-    var currentPage = parseInt(this.props.currentPage)
-    var dots = []
-    dots.push(<a onClick={this.handleClick}>1</a>)
-    dots.push(<a onClick={this.handleClick}>2</a>)
-    dots.push(<span className="gap">...</span>)
-    for (var i = (currentPage - 4); i < (currentPage + 5); i++) {
-      if (this.props.currentPage == i) {
-        var dot = <PageDot className="current" pageNumber={i}/>
-        dots.push(dot)
-      } else {
-        var dot = <PageDot handleClick={this.handleClick} pageNumber={i}/>
-        dots.push(dot)
-      }
-    }
-    dots.push(<span className="gap">...</span>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount -1}</a>)
-    dots.push(<a onClick={this.handleClick}>{this.props.pageCount}</a>)
-    return(dots)
-  },
-
-  renderDotsLastPage() {
-    var pageCount   = parseInt(this.props.pageCount)
-    var currentPage = parseInt(this.props.currentPage)
-    var dots = []
-    dots.push(<a onClick={this.handleClick}>1</a>)
-    dots.push(<a onClick={this.handleClick}>2</a>)
-    dots.push(<span className="gap">...</span>)
-    for (var i = pageCount - 8; i <= pageCount; i++) {
-      if (currentPage == i) {
-        var dot = <PageDot className="current" pageNumber={i}/>
-        dots.push(dot)
-      } else {
-        var dot = <PageDot handleClick={this.handleClick} pageNumber={i}/>
-        dots.push(dot)
-      }
+    if (pageCount === currentPage) {
+      dots.push(<NextGapOrPrev  disabled={true} pageDirection="Next →"/>)
+    } else if (pageCount - 7 <= currentPage) {
+      dots.push(<NextGapOrPrev changePageByOne={this.changePageByOne} pageDirection="Next →"/>)
+    } else {
+      dots.push(<NextGapOrPrev pageDirection="..."/>)
+      dots.push(<PageDot handleClick={this.handleClick} pageNumber={this.props.pageCount -1}/>)
+      dots.push(<PageDot handleClick={this.handleClick} pageNumber={this.props.pageCount}/>)
+      dots.push(<NextGapOrPrev changePageByOne={this.changePageByOne} pageDirection="Next →"/>)
     }
     return(dots)
   },
-
-
-
 
   render(){
     var pageCount   = parseInt(this.props.pageCount)
     var currentPage = parseInt(this.props.currentPage)
-    if (currentPage <= 1) {
+    if (currentPage <= 8) {
       return (
         <div>
-          <span className="previous_page disabled">← Previous</span>
-          {this.renderDotsFromZero()}
-          <a className="next_page " onClick={this.changePageByOne}>Next →</a>
-        </div>
-      )
-    } else if (currentPage <= 8) {
-      return (
-        <div>
-          <a className="previous_page " onClick={this.changePageByOne}>← Previous</a>
-          {this.renderDotsUnderEight()}
-          <a className="next_page " onClick={this.changePageByOne}>Next →</a>
+          {this.renderPagination(1,10)}
         </div>
       )
     } else if (currentPage >= 9 && currentPage <= pageCount - 8) {
       return (
         <div>
-          <a className="previous_page " onClick={this.changePageByOne}>← Previous</a>
-          {this.renderDotsAtNine()}
-          <a className="next_page " onClick={this.changePageByOne}>Next →</a>
-        </div>
-      )
-    }  else if (currentPage ==  pageCount) {
-      return(
-        <div>
-          <a className="previous_page " onClick={this.changePageByOne}>← Previous</a>
-          {this.renderDotsLastPage()}
-          <span className="next_page disabled ">Next →</span>
+          {this.renderPagination(currentPage - 4, currentPage + 5)}
         </div>
       )
     } else if (currentPage >= pageCount - 8) {
       return (
         <div>
-          <a className="previous_page " onClick={this.changePageByOne}>← Previous</a>
-          {this.renderDotsLastPage()}
-          <a className="next_page " onClick={this.changePageByOne}>Next →</a>
+          {this.renderPagination(pageCount - 8, pageCount + 1)}
         </div>
       )
     }
--- app/assets/javascripts/components/next_gap_or_prev.js.jsx
+++ app/assets/javascripts/components/next_gap_or_prev.js.jsx
@@ -13,7 +13,7 @@ var NextGapOrPrev = React.createClass({
     // could break apart if elseif into two if and keep it drier...
 
     if (pageDirection == "...") {
-      return (<span classname="gap">...</span> )
+      return (<span className="gap">...</span> )
     } else if (this.props.disabled) {
       var direction =  pageDirection.split(" ")[1].toLowerCase() == "previous" ? "previous" : "next"
       var pageClassName =  direction + "_page"
